4f734c6bbe319aec0f1a09a1f775ad4562b3dd8f
madarow <mathieu.lauffenburger.dev@gmail.com>
2017-01-08 15:22:06 +0100

    prepar demo circuit

--- src/components/CommunityGameList.vue
+++ src/components/CommunityGameList.vue
@@ -1,87 +1,26 @@
 <template lang="html">
-  <div class="decouvrir">
-    <div class="topContent">
-      <h1>À toi de jouer !</h1>
-      <p>C’est ici que tu trouveras les jeux et applications créés avec Declick
-         que les autres utilisateurs ont mis en ligne. Tu peux bien sûr les
-         essayer à l’infini ! Tu pourras aussi voir le code utilisé pour chacun
-         de ces jeux et t’en inspirer pour créer les tiens. Partage ensuite tes
-          créations et participe à la communauté Declick !</p>
-    </div>
-    <h2>Restitution Declick 2016</h2>
-    <ul class="gameList">
-      <li class="listElement" v-for="game in games">
-        <a :href="game.execute" target="_blank">
-        <img :src="game.imgSrc" alt="" /><br>
-            {{game.name}}<br>
-            {{game.createAt}}
-        </div>
-        </a>
-      </li>
-    </ul>
-
+  <div id="contentContainerBienvenue">
   </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      games: [
-        { name:'Donut Adventures (Les donuts)',
-          createAt: 'Collège Edouard Pailleron, Paris',
-          imgSrc: 'http://pages.declick.net/data/uploads/jeux/jeu1.png',
-          execute: 'http://create.declick.net/server/web/project/execute/1463'
-        },
-        { name:'Toilet Time (Toilets Studio)',
-          createAt: 'Collège Bernard Palissy, Paris',
-          imgSrc: 'http://pages.declick.net/data/uploads/jeux/jeu2.png',
-          execute: 'http://create.declick.net/server/web/project/execute/1486'
-        },
-        { name:'Lola & Ben (Forest Studio)',
-          createAt: 'Collège Bernard Palissy, Paris',
-          imgSrc: 'http://pages.declick.net/data/uploads/jeux/jeu3.png',
-          execute: 'http://create.declick.net/server/web/project/execute/1485'
-        },
-        { name:'The Lost Way (Griffondor)',
-          createAt: 'Collège Bernard Palissy, Paris',
-          imgSrc: 'http://pages.declick.net/data/uploads/jeux/jeu4.png',
-          execute: 'http://create.declick.net/server/web/project/execute/1487'
-        }]
-      }
+  created(){
+    this.$http.get('http://localhost/getSimpleCms/index.php?id=bienvenue-sur-declick').then((responce) => {
+      $('#contentContainerBienvenue').append($.parseHTML(responce.body))
+    })
   }
-
 }
 </script>
 
 <style lang="css">
-.decouvrir{
-  font-family: "DejaVu Sans",Helvetica,Arial,sans-serif;
-}
-  .listElement{
-    display: inline-block;
-    text-align: center;
-    width: 49%;
-    margin: 0 auto;
-  }
-  .decouvrir h2{
-    font-size: 19pt;
-    line-height: 37px;
-    letter-spacing: -1px;
-    margin: 100px 0 90px 0;
-    text-transform: uppercase;
-    font-family: 'rubik';
-    color: #FFFFFF;
-    background-color: #480A2A;
-    text-align: center;
-    position: relative;
-  }
-  .topContent{
+#content{
+    font-size: 13px;
     color: #480A2A;
-    text-align: left;
-    margin: 0 100px
-  }
-  .topContent h1{
+    text-align: center;
+}
+
+#content h1 {
     font-size: 24pt;
     line-height: 40px;
     letter-spacing: -2px;
@@ -89,19 +28,35 @@ export default {
     text-align: left;
     text-transform: uppercase;
     font-family: "rubik";
+    text-align: center;
     font-weight: 500;
-  }
-  .topContent p {
+}
+#content .page-text {
     font-size: 16px;
     line-height: 22px;
-  }
-  .listElement a {
-    display: block;
-text-decoration: none;
-color: #480a2a;
-margin: 10px 0;
-text-align: center;
-font-size: 16px;
-line-height: 22px;
-  }
+}
+#sommaire {
+  text-align: left;
+    margin: 7px;
+    box-sizing: border-box;
+    border-spacing: 15px;
+}
+#content table td {
+    vertical-align: top;
+}
+
+#sommaire td {
+    padding-right: 15px;
+    position: relative;
+}
+#content h3 {
+    font-family: arial;
+    font-size: 18px;
+    margin: 20px 0 7px 0;
+    text-transform: uppercase;
+    font-weight: 600;
+}
+#content #sommaire td p {
+    margin: 0 0 50px 0;
+}
 </style>
--- src/components/DeclickMap.vue
+++ src/components/DeclickMap.vue
@@ -1,10 +1,7 @@
 <template lang="html">
   <div id="declickMap">
+    <!--<div id="text"></div>-->
     <canvas id="map"></canvas>
-    <button @click="updateState()">Mise à jour étape 1</button>
-    <button @click="updateCurrent()">Mise à jour étape courante</button>
-    <div id="text"></div>
-    <button @click="show()">Apparaître</button>
   </div>
 
 </template>
@@ -16,7 +13,7 @@ export default {
   data () {
     return {
       passed: false,
-      current: 7,
+      current: 0,
       json: {
           "data": "M25.255,63.302c32.97-3.167,63.481-17.886,96.522-21.262c31.374-3.206,62.175-4.643,94.51-4.328c45.235,0.439,113.158,22.029,128.641,66.525c10.522,30.243,11.189,71.242-8.883,97.309c-23.825,30.939-55.704,40.616-89.128,60.174c-39.078,22.867-39.078,22.867-75.06,47.575c-35.261,24.213-87.611,59.112-92.27,102.089c-2.703,24.943,7.114,54.339,21.787,74.96c28.008,39.363,65.192,80.229,111.434,101.433c55.587,25.485,112.499,36.663,173.848,39.621c57.567,2.778,111.682,2.036,162.889-24.55c39.235-20.368,69.451-54.185,69.977-97.74c0.375-31.134-11.793-55.753-22.851-84.559c-11.122-28.972-16.033-58.073-15.71-88.649c0.292-27.768,11.396-54.101,35.71-70.92c25.972-17.969,54.402-25.999,72.833-52.986c33.826-49.535,12.82-134.523,70.377-170.015c65.75-40.539,163.432,7.745,209.688,53.859c34.891,34.786,57.88,91.84,58.417,139.253c0.237,20.913,3.027,44.543-1.792,64.917c-5.568,23.546-19.611,34.563-39.299,48.274c-40.057,27.902-78.061,62.405-88.834,109.851c-4.686,20.627-9.176,46.802,1.037,66.749c6.909,13.499,19.683,26.981,31.945,36.569c52.711,41.21,131.881,44.271,196.91,44.396c84.241,0.162,148.639-52.938,177.451-124.24c23.287-57.621,31.08-113.144,31.07-174.281c-0.009-49.22-22.069-102.894-12.887-151.773c4.817-25.648,43.632-57.636,68.898-66.53c30.76-10.827,67.95,3.379,96.806,13.133",
           "color": "#CFBFC7",
@@ -26,7 +23,7 @@ export default {
       steps: [
           {
               "name": "CREATION D'UN COMPTE",
-              "passed": true,
+              "passed": false,
               "id": 0,
               "steps": [
                   {
@@ -36,59 +33,59 @@ export default {
                   },
                   {
                       "name":"Objectifs",
-                      "passed":true,
+                      "passed":false,
                       "id": 2
                   },
                   {
                       "name":"Instructions",
-                      "passed":true,
+                      "passed":false,
                       "id": 3
                   },
                   {
                       "name":"Informations utiles",
-                      "passed":true,
+                      "passed":false,
                       "id": 4
                   }
               ]
           },
           {
               "name": "QUELQUES RAPPELS",
-              "passed": true,
+              "passed": false,
               "id": 5,
               "steps": [
                   {
                       "name":"Minuscules",
-                      "passed":true,
+                      "passed":false,
                       "id": 6
                   },
                   {
                       "name":"Petites et grandes lettres",
-                      "passed":true,
+                      "passed":false,
                       "id": 7
                   },
                   {
                       "name":"Majuscules",
-                      "passed":true,
+                      "passed":false,
                       "id": 8
                   },
                   {
                       "name":"Des signes mathématiques",
-                      "passed":true,
+                      "passed":false,
                       "id": 9
                   },
                   {
                       "name":"La formule",
-                      "passed":true,
+                      "passed":false,
                       "id": 10
                   },
                   {
                       "name":"Point final",
-                      "passed":true,
+                      "passed":false,
                       "id": 11
                   },
                   {
                       "name":"Le point",
-                      "passed":true,
+                      "passed":false,
                       "id": 12
                   }
               ]
@@ -99,22 +96,22 @@ export default {
               "steps": [
                   {
                       "name":"Création",
-                      "passed":true,
+                      "passed":false,
                       "id": 14
                   },
                   {
                       "name":"Le robot est créé",
-                      "passed":true,
+                      "passed":false,
                       "id": 15
                   },
                   {
                       "name":"En avant...",
-                      "visited":true,
+                      "visited":false,
                       "id": 16
                   },
                   {
                       "name":"...en arrière",
-                      "visited":true,
+                      "visited":false,
                       "id": 17
                   },
                   {
@@ -294,12 +291,6 @@ export default {
     }
   },
   methods: {
-
-    updateState () {
-      var _this = this
-        this.map.updateState([{id:1, passed:_this.passed}]);
-        this.passed = !this.passed
-    },
     updateCurrent() {
         var _this = this
         this.map.updateState([{id:_this.current, passed:true}]);
@@ -316,7 +307,7 @@ export default {
     $(function(){
         _this.map = new DeclickMap();
         _this.map.init("map", bot, function(index) {
-            $("#text").text("Ouverture de l'item n°" + index);
+            // $("#text").text("Ouverture de l'item n°" + index);
         }, function () {
             // Load path
             _this.map.loadPathFromUI(_this.json, function() {
@@ -324,25 +315,24 @@ export default {
                 // Option 1 from JSON file
                 _this.map.loadStepsFromUI(_this.steps, function() {
                     // Set current step
-                      _this.map.setCurrentStep(6);
+                      //_this.map.setCurrentStep(1);
+                        _this.show()
                 });
-                // Option 2 from JS Object
-
-                  /*$.getJSON("steps.json", function(pathData) {
-                      map.loadSteps(_this.steps);
-                      map.setCurrentStep(6);
-                 });*/
-
             });
         });
     });
+  },
+  watch:{
+    'map.targetCurrent'(){
+      console.log(this.map.targetCurrent);
+    }
   }
 }
 </script>
 
 <style lang="css">
 #declickMap {
-    background-color:#000000;
+    background-color:#46102A;
 }
 #map {
     width:100%;
