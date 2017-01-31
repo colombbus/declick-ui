43f9e111590b757af7534cc25619d1c4ec947af2
bonpra <benoit.prady@colombbus.org>
2017-01-31 22:50:52 +0100

    Started implementing link with tasks

--- src/components/progress/CircuitRun.vue
+++ src/components/progress/CircuitRun.vue
@@ -3,14 +3,37 @@
 </template>
 <script>
 import declickConfig from '../../assets/config/declick.js'
-
+import Channel from 'exports-loader?Channel!jschannel/src/jschannel.js'
+window.Channel = Channel;
+import pem from 'exports-loader?TaskProxyManager&Platform!pem-platform/task-xd-pr.js'
+var task = false;
 export default {
   computed: {
         urlLearn () {
         return declickConfig.url.client + 'learn.html#id=4&token=' + this.$store.state.authorizations
         }
+    },
+    mounted() {
+        pem.Platform.prototype.showView = function(views, success, error) {
+            window.console.log("received show view");
+            success();
+            /*
+                task.reloadAnswer({score : 0,value : "coucou"}, () => {
+                console.log("done.");
+            });
+            */
+
+        };
+        pem.TaskProxyManager.getTaskProxy("declick-client-learn",(ref) => {
+            task = ref;
+            window.console.log("setting platform");
+            pem.TaskProxyManager.setPlatform(task, new pem.Platform());
+            window.console.log("platform set");
+        });
     }
 }
+
+
 </script>
 <style lang="css">
 .fullscreen-iframe {
