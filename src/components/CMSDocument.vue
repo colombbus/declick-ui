085b640ea99e543ed3bed87e969610a4bdbbd34e
Yovar <yovar@rougain.com>
2017-02-09 15:59:31 +0100

    create CMSDocument

---
+++ src/components/CMSDocument.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="cms-document-loading-message">
+      <img src="assets/img/spinner.gif">
+      <p>Chargement...</p>
+    </div>
+    <div class="cms-document-content"></div>
+  </div>
+</template>
+
+<script>
+import config from 'assets/config/declick'
+
+export default {
+  props: [
+    'id'
+  ],
+  created () {
+    let url = config.url.cms +
+      'api/1/tables/pages/rows/' +
+      this.id +
+      '?access_token=jWNoVhWCng6odNLK'
+    this.$http.get(url).then((response) => {
+      $(this.el).find('.cms-document-loading-message').fadeOut()
+      $(this.el).find('.cms-document-loading-message').append(
+        $.parseHTML(response.body.content)
+      ).fadeIn()
+    })
+  }
+}
+</script>
+
+<style>
+.cms-document-loading-message img {
+  height: 50px;
+}
+
+.cms-document-loading-message p {
+  font-weight: bold;
+}
+</style>
