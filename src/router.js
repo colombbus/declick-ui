210149b54dbe2bfd34ea7d036d0ad8b0de9d0bb2
Yovar <yovar@rougain.com>
2017-02-09 15:17:10 +0100

    clean-up routes

--- src/router.js
+++ src/router.js
@@ -3,7 +3,6 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-import Create from 'components/Create'
 import UserProfile from 'components/UserProfile'
 import ResourceCreation from 'components/ResourceCreation'
 import CommunityGameList from 'components/CommunityGameList'
@@ -27,54 +26,97 @@ var routes = [
   {
     path: '/',
     component: PageDeclick,
-    meta: { title: 'Accueil' }
-  },
-  {
-    path: '/explore',
-    component: CommunityGameList,
-    meta: { title: 'Découvrir' }
-  },
-  {
-    path: '/progress', component: Progress, meta: { title: 'Progresser' }, children: [
-      {path: '', component: CircuitList, meta: { title: 'Liste des parcours' }},
-      {path: 'circuit', component: Circuit, children: [
-        {path: '', component: DeclickMap, meta: { title: 'Parcours' }},
-        {path: 'run', meta: { title: 'Étape' }}
-      ]}
+    meta: { title: 'Accueil' },
+    children: [
+      {
+        path: '/explore',
+        component: CommunityGameList,
+        meta: { title: 'Découvrir' }
+      },
+      {
+        path: '/progress',
+        component: Progress,
+        meta: { title: 'Progresser' },
+        children: [
+          {
+            path: '',
+            component: CircuitList,
+            meta: { title: 'Liste des parcours' }
+          },
+          {
+            path: 'circuit',
+            component: Circuit,
+            children: [
+              {
+                path: '',
+                component: DeclickMap,
+                meta: { title: 'Parcours' }
+              },
+              {
+                path: 'run',
+                meta: { title: 'Étape' }
+              }
+            ]
+          }
+        ]
+      },
+      {
+        path: '/users/me',
+        component: UserProfile,
+        meta: { title: 'Mon Profil' }
+      },
+      {
+        path: '/resources/new',
+        component: ResourceCreation,
+        meta: { title: 'Création de ressource' }
+      },
+      {
+        path: '/create',
+        name: 'Créer',
+        meta: {
+          title: 'Progresser',
+          useFullscreenMode: true
+        }
+      },
+      {
+        path: '/administration',
+        component: Administration,
+        meta: { title: 'Administration' },
+        children: [
+          {
+            path: 'circuits',
+            component: AdministrationCircuitList,
+            meta: { title: 'Liste des parcours' }
+          },
+          {
+            path: 'circuits/:id',
+            component: CircuitEditor,
+            meta: { title: 'Modification de parcours' }
+          },
+          {
+            path: 'lists/:type',
+            component: ListAdmin,
+            meta: { title: 'liste' }
+          },
+          {
+            path: 'current',
+            component: CurrentProjectInformations,
+            meta: { title: 'Panneau d\'exemple' }
+          },
+          {
+            path: 'edit/:id',
+            component: EditProject,
+            meta: { title: 'Edition' }
+          },
+          {
+            path: 'list/:type',
+            component: MyList,
+            meta: { title: 'list' }
+          }
+        ]
+      }
     ]
-  },
-  {
-    path: '/users/me',
-    component: UserProfile,
-    meta: { title: 'Mon Profil' }
-  },
-  { path: '/resources/new', component: ResourceCreation, meta: { title: 'Création de ressource' }},
-
-  {
-    path: '/create',
-    name: 'Créer',
-    meta: {
-      title: 'Progresser',
-      useFullscreenMode: true
-    }
-  },
-
-  { path: '/administration', component: Administration, meta: { title: 'Administration' }, children: [
-    {
-      path: 'circuits',
-      component: AdministrationCircuitList,
-      meta: { title: 'Liste des parcours' }
-    },
-    {
-      path: 'circuits/:id',
-      component: CircuitEditor,
-      meta: { title: 'Modification de parcours' }
-    },
-      { path: 'lists/:type', component: ListAdmin, meta: { title: 'liste' } },
-      { path: 'current', component: CurrentProjectInformations, meta: { title: 'Panneau d\'exemple' } },
-      { path: 'edit/:id', component: EditProject, meta: { title: 'Edition' } },
-      { path: 'list/:type', component: MyList, meta: { title: 'list' } }
-    ]}
+  }
 ]
 
 export default new VueRouter({
