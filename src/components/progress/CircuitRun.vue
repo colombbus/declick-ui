a4a96310b51478f621877763705be9b98c7b9fb2
bonpra <benoit.prady@colombbus.org>
2017-02-23 12:50:55 +0100

    Get validate messages from declick client

--- src/components/progress/CircuitRun.vue
+++ src/components/progress/CircuitRun.vue
@@ -4,6 +4,8 @@
 <script>
 import declickConfig from '../../assets/config/declick.js'
 import Channel from 'exports-loader?Channel!jschannel/src/jschannel.js'
+import { mapState } from 'vuex'
+
 window.Channel = Channel;
 import pem from 'exports-loader?TaskProxyManager&Platform!pem-platform/task-xd-pr.js'
 var task = false;
@@ -14,19 +16,42 @@ export default {
     },
     computed: {
         urlLearn () {
-            if (this.$store.state.current_step_url == '') {
+            if (this.current_step_url == '') {
                 this.$store.dispatch('set_current_step_url',declickConfig.url.client +'learn.html#id=4')                
             }
-            return this.$store.state.current_step_url+"&token="+ this.$store.state.authorizations+"&channelId=declick"
-        }
+            return this.current_step_url+"&token="+ this.authorizations+"&channelId=declick"
+        },
+        ...mapState(['current_step_url', 'current_step_index', 'authorizations'])
     },
     mounted() {
         pem.Platform.prototype.showView = function(views, success, error) {
-                console.log("sow view received")
+                console.log("show view received")
                 task.reloadAnswer(JSON.stringify({score : 0,value : "coucou"}), () => {
                 success()
             })
         }
+        var self = this
+        pem.Platform.prototype.validate = function(mode, success, error) {
+            console.log("validate received")
+            task.getAnswer((answer)=> {
+                console.log("answer received")
+                console.debug(answer)
+                // TODO: store answer
+                // TODO: set map accordingly
+                switch (mode) {
+                    case "stay": 
+                        console.log("mode: stay")
+                        break
+                    case "nextImmediate":
+                        console.log("mode: nextImmediate")
+                        self.$store.dispatch('set_current_step_index',this.current_step_index + 1)
+                        break
+                }
+                success()
+
+            })
+        }
+
         pem.TaskProxyManager.getTaskProxy("declick-client-learn",(ref) => {
             task = ref
             pem.TaskProxyManager.setPlatform(task, new pem.Platform(task))
