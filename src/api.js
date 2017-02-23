01efc6b103ea4b29d33bbe0668864c06b8f98b96
Yovar <yovar@rougain.com>
2017-02-23 14:11:43 +0100

    change circuit steps format

--- src/api.js
+++ src/api.js
@@ -4,17 +4,19 @@ import config from 'assets/config/declick'
 
 var apiUrl = config.url.api.slice(0, -1)
 
-function convertNodes (parentNode, nodes) {
+function convertNodes (parentNode, nodes, state) {
   let childrenNodes = nodes.filter(node => node.parent_id === parentNode.id)
   let parentStep = {
-    id: parentNode.link,
+    index: state.index++,
+    id: parentNode.id,
     name: parentNode.name,
+    url: parentNode.link,
     passed: false
   }
   if (childrenNodes.length > 0) {
     parentStep.steps = []
     for (let childNode of childrenNodes) {
-      parentStep.steps.push(convertNodes(childNode, nodes))
+      parentStep.steps.push(convertNodes(childNode, nodes, state))
     }
   }
   return parentStep
@@ -26,7 +28,7 @@ export default {
       url: `${apiUrl}/circuits/${circuitId}/nodes`,
       success: nodes => {
         let rootNode = nodes.filter(node => node.parent_id === null)[0]
-        let tree = convertNodes(rootNode, nodes).steps
+        let tree = convertNodes(rootNode, nodes, { index: 0 }).steps
         callback(tree)
       }
     })
