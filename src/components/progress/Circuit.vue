f2cc6262348123b9c8f75af69fa418a1b515ae6b
Yovar <yovar@rougain.com>
2017-02-27 12:35:58 +0100

    add Circuit keep-alive

--- src/components/progress/Circuit.vue
+++ src/components/progress/Circuit.vue
@@ -1,8 +1,9 @@
-<template lang="html">
+<template>
+  <keep-alive>
     <router-view></router-view>
+  </keep-alive>
 </template>
+
 <script>
 export default {}
 </script>
-<style lang="css">
-</style>
