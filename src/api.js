d2a47308b3d951fa74a61dc2ec542bdfc8abac9b
Yovar <yovar@rougain.com>
2017-03-16 14:15:49 +0100

    add step results fetching

--- src/api.js
+++ src/api.js
@@ -1,8 +1,25 @@
 /* globals $ */
 
+import R from 'ramda'
+import store from './store'
 import config from 'assets/config/declick'
 
-var apiUrl = config.url.api.slice(0, -1)
+function getResults (callback) {
+  if (!store.state.authenticatedUser) {
+    return callback([])
+  }
+  $.ajax({
+    url: `${config.apiUrl}v1/users/${store.state.authenticatedUser.id}/results`,
+    success: results => {
+      callback(results)
+    },
+    beforeSend: function (request) {
+      request.setRequestHeader(
+        'Authorization', `Token ${store.state.authorizations}`
+      )
+    }
+  })
+}
 
 function convertNodes (parentNode, nodes, state) {
   let childrenNodes = nodes.filter(node => parseInt(node.parent_id) === parentNode.id)
@@ -26,12 +43,31 @@ function convertNodes (parentNode, nodes, state) {
 export default {
   retrieveSteps (circuitId, callback) {
     $.ajax({
-      url: `${apiUrl}/circuits/${circuitId}/nodes`,
+      url: `${config.apiUrl}v1/circuits/${circuitId}/nodes`,
       success: nodes => {
         let rootNode = nodes.filter(node => node.parent_id === null)[0]
         let tree = convertNodes(rootNode, nodes, { position: 0 }).steps
-        callback(tree)
+        getResults(results => {
+          let steps = flattenTree(tree, 'steps')
+          steps.forEach(step => {
+            let result = R.find(R.propEq('step_id', step.id), results)
+            if (result) {
+              step.visited = true
+              step.passed = result.passed !== 0
+              step.solution = result.solution
+            }
+          })
+          callback(tree)
+        })
       }
     })
   }
 }
+
+function flattenTree (tree, key) {
+  const level = element => element[key]
+    ? [element, element[key].map(level)]
+    : element
+  return R.flatten(tree.map(level))
+}
+
