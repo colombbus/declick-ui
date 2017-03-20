16e926679ca9a083399e322dd8d16650415867e8
Yovar <yovar@rougain.com>
2017-03-20 14:27:41 +0100

    clean up AuthenticatedUserBox

--- src/components/AuthenticatedUserBox.vue
+++ src/components/AuthenticatedUserBox.vue
@@ -1,45 +1,35 @@
-<template>
-  <div class="authenticated-user-box">
-    <router-link
-      to="/users/me"
-    >{{authenticatedUser.username}}</router-link>
-    <router-link to="/administration">
-      <span
-        class="glyphicon glyphicon-wrench"
-        aria-hidden="true"
-      ></span>
-    </router-link>
-  </div>
+<template lang="pug">
+.self
+  router-link(to='/me') {{authenticatedUser.username}}
+  router-link(to='/administration')
+    span.glyphicon.glyphicon-wrench
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import {mapState} from 'vuex'
 
 export default {
   computed: mapState(['authenticatedUser'])
 }
 </script>
 
-<style>
-.authenticated-user-box {
-  height: 38px;
-  width: 150px;
-  text-align: center;
-  line-height: 35px;
-  background-color: #642D46;
-}
+<style lang="sass" scoped>
+a
+  padding: 0
+  font-family: Rubik, sans-serif
+  font-size: 18.6667px
+  color: white
+  outline: 0
+  cursor: pointer
 
-.authenticated-user-box a {
-  padding: 0;
-  font-family: Rubik, sans-serif;
-  font-size: 18.6667px;
-  color: white;
-  outline: 0;
-  cursor: pointer;
-}
+.self
+  height: 38px
+  width: 150px
+  text-align: center
+  line-height: 35px
+  background-color: #642D46
 
-.authenticated-user-box .glyphicon {
-  position: relative;
-  top: 3px;
-}
+.glyphicon
+  position: relative
+  top: 3px
 </style>
