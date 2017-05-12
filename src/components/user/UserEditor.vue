b23ed8cfb5e9ff91808b8830e3567e9df7293183
bonpra <benoit.prady@colombbus.org>
2017-05-12 03:08:34 +0200

    offline mode support for UserEditor

--- src/components/user/UserEditor.vue
+++ src/components/user/UserEditor.vue
@@ -22,7 +22,17 @@ div
 </template>
 
 <script>
-import Api from 'src/api'
+import Remote from 'src/api'
+import config from 'assets/config/declick'
+import Storage from 'src/storage'
+
+let Api
+
+if (config.offline) {
+  Api = Storage
+} else {
+  Api = Remote
+}
 
 export default {
   props: ['id'],
