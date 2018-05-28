4cd1b7736e61f56aaf2e109b30c02e73499ec154
Yovar <yovar@rougain.com>
2018-05-28 06:58:04 +0000

    add various comments in store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -18,3 +18,35 @@ export default new Vuex.Store({
   mutations,
   actions
 })
+
+
+/*
+- token
+- user
+- course
+- stage
+- project
+
+# methods (manual)
+
+- register
+- logIn
+- restoreSession
+  // stocker le token et afficher une apparence de transition
+- logOut
+
+- selectCourse
+- selectStage
+- previousStage
+- nextStage
+- submitAttempt
+
+- createProject
+- selectProject
+- updateProject
+
+# methods (subroutines) // Est-ce utile d'en faire des actions ? voir cas par cas ou même ne pas mettre dans le store du tout
+
+- getCourseAttempts
+- getUserProjects
+*/
